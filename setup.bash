5adf57f79ff3a812c43aa3117eed2ebaec46a7cb
Elliott-V <elliottv@sasktel.net>
2026-03-24 13:03:07 -0600

    Added parameters for executable in setup.bash

--- setup.bash
+++ setup.bash
@@ -3,14 +3,20 @@
 # !! DONT RUN YET
 
 NUM_SERVERS=5 # Must be the same as in types.h
-EXECUTBLE="./server"
+EXECUTABLE="./server"
+HOST_NAME=$(hostname)
+PORT_NUM=33000
 
 #ptyxis --execute "bash -c \"$EXECTUABLE; exec bash\"" &
 
-for ((i=0; i<$NUM_SERVERS; i++)); do
-	$EXECUTABLE &
+getServer() {
+	id=$(((($1 + $2) % 5) + 1))
+	echo "$id" "$HOST_NAME" "$((PORT_NUM + id))"
+}
+
+for ((i = 1; i < (NUM_SERVERS + 1); i++)); do
+	$EXECUTABLE $i $((PORT_NUM + i)) $(getServer $i 0) $(getServer $i 1) $(getServer $i 2) $(getServer $i 3) &
 	echo "process $i started with pid $!"
 done
 
 echo "All processes started"
-
