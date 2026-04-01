48439dcaf7e009742802e62af4b0897ba3284814
Elliott-V <elliottv@sasktel.net>
2026-03-31 22:12:03 -0600

    updated RequestVote function headers and created arg struct

--- request_vote.h
+++ request_vote.h
@@ -1,11 +1,23 @@
 #ifndef REQUEST_VOTE_H
 #define REQUEST_VOTE_H
 
+#include <types.h>
+
+/* TODO: */
+typedef struct {
+	int sockfd;
+	RPCMsg msg;
+} RequestVoteArgs;
+
 typedef struct {
 	int term; /* current term */
 	int voteGranted; /* 0 fail 1 success */
 } RequestResult;
 
-RequestResult *RequestVote(int term, int candidateId, int lastlogIndex, int lastLogTerm);
+/* TODO: */
+void *RequestVoteThread(void *ptr);
+
+/* TODO: */
+RequestResult *RequestVote(int sockfd, int rpcType, int term, int candidateId, int lastLogIndex, int lastLogTerm);
 
 #endif
