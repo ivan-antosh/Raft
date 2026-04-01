543f011858653a23008d2d624da1011ca07d6d99
Elliott-V <elliottv@sasktel.net>
2026-03-31 22:13:15 -0600

    setup RequestVote send and rec, created thread heandler function

--- request_vote.c
+++ request_vote.c
@@ -1,6 +1,35 @@
-#include <request_vote.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-RequestResult *RequestVote(int term, int candidateId, int lastlogIndex, int lastLogTerm) {
-	return NULL;
+#include "request_vote.h"
+
+void *RequestVoteThread(void *ptr) {
+	RequestVoteArgs *args = (RequestVoteArgs *) ptr;
+
+	RequestResult *result = RequestVote(args->sockfd, args->msg.rpcType, args->msg.term, args->msg.id, args->msg.logIndex, args->msg.logTerm);
+
+	pthread_exit(result);
+}
+
+//RequestResult *RequestVote(int sockfd, int term, int id, int logIndex, int logTerm) {
+RequestResult *RequestVote(int sockfd, int rpcType, int term, int id, int logIndex, int logTerm) {
+	RPCMsg msg = {htons(rpcType), htons(term), htons(id), htons(logIndex), htons(logTerm)};
+	RequestResult *reply = malloc(sizeof(RequestResult));
+
+	if(send(sockfd, &msg, sizeof(msg), 0) == -1) {
+		perror("RequestVote - send");
+		exit(1);
+	}
+
+	printf("RequestVote send to fd %d\n", sockfd);
+
+	if((recv(sockfd, reply, sizeof(RequestResult), 0)) == -1) {
+		perror("RequestResult - recv");
+		exit(2);
+	}
+
+	return reply;
 }
