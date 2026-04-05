c864f2aec26a56a0c152878f04b4f6e9ee842f2d
Elliott-V <elliottv@sasktel.net>
2026-04-04 21:44:56 -0600

    refactor proxy main

--- proxy.c
+++ proxy.c
@@ -2,52 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
+#include <poll.h>
 
 #include "proxy.h"
 #include "helper.h"
+#include "types.h"
 
-/* Thread to handle a single bidirectional proxy connection */
+/* Thread to handle a single bidirectional traffic for a single established connection */
 void *handle_connection(void *arg) {
 	proxy_args_t *args = (proxy_args_t*)arg;
 	int client_fd = args->client_fd;
 	int server_fd;
+
+	printf("TEST THREAD\n");
 
 	free(args);
 	return NULL;
 }
 
 int main(int argc, char *argv[]) {
-	int listener;
+	struct pollfd listeners[NUM_SERVERS];
+	proxy_args_t targetArgs[NUM_SERVERS];
 
-	if (argc != 4) {
+	if (argc != (3 * NUM_SERVERS)+1) {
 		printf("Error: invalid number of arguments\n"
-			"Usage: ./proxy <listen port> <target host> <target port>\n");
+			"Usage: ./proxy (<listen port> <target host> <target port>) * (%d servers)\n", NUM_SERVERS);
 		return 1;
 	}
 
-	char *listen_port = argv[1];
-	char *target_host = argv[2];
-	char *target_port = argv[3];
+	srand(time(NULL));
 
-	listener = get_listener_socket(listen_port);
+	/* Setup listening socket for each route */
+	for (int i = 0; i < NUM_SERVERS; i++) {
+		char *listen_port = argv[(i*3)+1];
+		strcpy(targetArgs[i].target_host, argv[(i*3)+2]);
+		strcpy(targetArgs[i].target_port, argv[(i*3)+3]);
 
-	printf("Proxy listening on port %s -> forwarding to %s:%s\n", listen_port, target_host, target_port);
+		listeners[i].fd = get_listener_socket(listen_port);
+		listeners[i].events = POLLIN;
+
+		printf("Proxy listening on port %s -> forwarding to %s:%s\n", 
+				 listen_port, targetArgs[i].target_host, targetArgs[i].target_port);
+	}
 
 	for (;;) {
-		struct sockaddr_in client_addr;
-		socklen_t client_len = sizeof(client_addr);
-		int client_fd = accept(listener, (struct sockaddr*)&client_addr, &client_len);
+		if (poll(listeners, NUM_SERVERS, -1) > 0) {
+			for (int i = 0; i < NUM_SERVERS; i++) {
+				if (listeners[i].revents & POLLIN) {
+					int client_fd = accept(listeners[i].fd, NULL, NULL);
+					targetArgs[i].client_fd = client_fd;
 
-		if (client_fd >= 0) {
-			proxy_args_t *args = malloc(sizeof(proxy_args_t));
-			args->client_fd = client_fd;
-			strcpy(args->target_host, target_host);
-			strcpy(args->target_port, target_port);
-
-			pthread_t tid;
-			/* Spawn a thread for this connection and detach it so it cleans up its own memory */
-			pthread_create(&tid, NULL, handle_connection, args);
-			pthread_detach(tid);
+					if (client_fd >= 0) {
+						pthread_t tid;
+						/* Spawn a thread for this connection and detach it so it cleans up its own memory */
+						pthread_create(&tid, NULL, handle_connection, &targetArgs[i]);
+						pthread_detach(tid);
+					}
+				}
+			}
 		}
 	}
 
